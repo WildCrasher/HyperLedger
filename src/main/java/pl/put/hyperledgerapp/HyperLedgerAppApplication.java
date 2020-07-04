a53d671eaacbc5f00e2308ded5d99e028081b559
WildCrasher <dzikigeneral1996@o2.pl>
2020-07-04 19:16:37 +0200

    Initial commit.

---
+++ src/main/java/pl/put/hyperledgerapp/HyperLedgerAppApplication.java
@@ -0,0 +1,13 @@
+package pl.put.hyperledgerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperLedgerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyperLedgerAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pl/put/hyperledgerapp/TestClass.java
@@ -0,0 +1,14 @@
+package pl.put.hyperledgerapp;
+
+public class TestClass {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
