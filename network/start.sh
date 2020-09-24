0352644b4d537c0135518dbea2b2d1d093ef7a5d
Piotr Furmankiewicz <p.furmankiewicz@wildasoftware.pl>
2020-09-24 10:34:33 +0200

    Add copy of certs to start.sh

--- network/start.sh
+++ network/start.sh
@@ -3,3 +3,12 @@
 ./network.sh down
 ./network.sh up createChannel -ca
 ./network.sh deployCC -l java
+
+cp ./organizations/peerOrganizations/students.put.poznan.pl/connection-students.yaml ../organization/students/gateway/
+cp ./organizations/peerOrganizations/supervisors.put.poznan.pl/connection-supervisors.yaml ../organization/supervisors/gateway/
+
+cp ./organizations/peerOrganizations/students.put.poznan.pl/users/User1@students.put.poznan.pl/msp/signcerts/* ./organizations/peerOrganizations/students.put.poznan.pl/users/User1@students.put.poznan.pl/msp/signcerts/User1@students.put.poznan.pl-cert.pem
+cp ./organizations/peerOrganizations/students.put.poznan.pl/users/User1@students.put.poznan.pl/msp/keystore/* ./organizations/peerOrganizations/students.put.poznan.pl/users/User1@students.put.poznan.pl/msp/keystore/priv_sk
+
+cp ./organizations/peerOrganizations/supervisors.put.poznan.pl/users/User1@supervisors.put.poznan.pl/msp/signcerts/* ./organizations/peerOrganizations/supervisors.put.poznan.pl/users/User1@supervisors.put.poznan.pl/msp/signcerts/User1@supervisors.put.poznan.pl-cert.pem
+cp ./organizations/peerOrganizations/supervisors.put.poznan.pl/users/User1@supervisors.put.poznan.pl/msp/keystore/* ./organizations/peerOrganizations/supervisors.put.poznan.pl/users/User1@supervisors.put.poznan.pl/msp/keystore/priv_sk
