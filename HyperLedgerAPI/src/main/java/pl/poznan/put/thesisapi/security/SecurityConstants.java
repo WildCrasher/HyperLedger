c689f64e9f08316d0c813f53117741c8621de1af
wildcrasher <dzikigeneral1996@o2.pl>
2020-10-02 16:20:30 +0200

    Added url filter.

--- HyperLedgerAPI/src/main/java/pl/poznan/put/thesisapi/security/SecurityConstants.java
+++ HyperLedgerAPI/src/main/java/pl/poznan/put/thesisapi/security/SecurityConstants.java
@@ -6,4 +6,5 @@ public class SecurityConstants {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String LOGIN_URL = "/login";
+    public static final String REGISTER_URL = "/register";
 }
--- HyperLedgerAPI/src/main/java/pl/poznan/put/thesisapi/security/WebSecurity.java
+++ HyperLedgerAPI/src/main/java/pl/poznan/put/thesisapi/security/WebSecurity.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import pl.poznan.put.thesisapi.user.UserService;
 
 import static pl.poznan.put.thesisapi.security.SecurityConstants.LOGIN_URL;
+import static pl.poznan.put.thesisapi.security.SecurityConstants.REGISTER_URL;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -28,7 +29,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, LOGIN_URL).permitAll()
+                .antMatchers(HttpMethod.POST, LOGIN_URL, REGISTER_URL).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
